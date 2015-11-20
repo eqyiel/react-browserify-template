7ba51c4c853a35d559bf52b08cc1590e42ada2ed
Ruben Maher <r@rkm.id.au>
2015-11-20 18:33:10 +1030

    Remove cruft and bring it into 2015

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-/** @jsx React.DOM */
 var React = require('react');
+var ReactDOM = require('react-dom');
 var pkg = require('./package.json');
 
-React.renderComponent(<h1>{pkg.name}, brought to you by React!</h1>, document.body);
+ReactDOM.render(
+  <h1>{pkg.name}, brought to you by React!</h1>,
+  document.getElementById('app')
+);
